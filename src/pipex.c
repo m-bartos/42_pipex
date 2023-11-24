d08016e793dcedb1fc7e7438d0c7dba8f0096685
m-bartos <m.bartos96@seznam.cz>
2023-11-24 20:00:30 +0100

    Update: working pipex.c for 2 cmds
    Only 2 cmds woking. Code is not polished.

--- src/pipex.c
+++ src/pipex.c
@@ -6,11 +6,11 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 17:22:30 by mbartos           #+#    #+#             */
-/*   Updated: 2023/11/24 15:18:57 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/24 19:55:55 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pipex.h>
+#include "pipex.h"
 
 char	**get_paths(char **envp)
 {
@@ -96,15 +96,31 @@ int	main(int argc, char **argv, char **envp)
 		return (1);
 	}
 	fd_in_file = open(argv[1], O_RDONLY);
-	fd_out_file = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd_in_file == -1)
+	{
+		ft_putstr_fd("Problem opening first file\n", 2);
+		return (1);
+	}
+	fd_out_file = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd_out_file == -1)
+	{
+		ft_putstr_fd("Problem opening second file\n", 2);
+		return (1);
+	}
 
 	num_of_cmds = argc - 3;
 	cmd_paths = malloc(sizeof(char **) * (num_of_cmds + 1));
-	if(cmd_paths)
+	if(cmd_paths == NULL)
+	{
+		ft_putstr_fd("Malloc problem\n", 2);
 		return (1);
+	}
 	cmd = malloc(sizeof(char **) * (num_of_cmds + 1));
-	if(cmd)
+	if(cmd == NULL)
+	{
+		ft_putstr_fd("Malloc problem\n", 2);
 		return (1);
+	}
 	cmd_paths[num_of_cmds] = NULL;
 	cmd[num_of_cmds] = NULL;
 	paths = get_paths(envp);
@@ -120,41 +136,90 @@ int	main(int argc, char **argv, char **envp)
 	while (cmd_paths[0][i] && access(cmd_paths[0][i], X_OK) != 0)
 		i++;
 	if (cmd_paths[0][i] == NULL)
-		ft_putstr_fd("Wrong command1", 2);
-
+	{
+		ft_putstr_fd("Wrong command1\n", 2);
+		return (1);
+	}
 	j = 0;
 	while (cmd_paths[1][j] && access(cmd_paths[1][j], X_OK) != 0)
 		j++;
 	if (cmd_paths[1][j] == NULL)
-		ft_putstr_fd("Wrong command2", 2);
+	{
+		ft_putstr_fd("Wrong command2\n", 2);
+		return (1);
+	}
 
 /* This is not working: */
 	if (pipe(p_fd) == -1)
 	{
-		perror("Error creating pipe");
+		perror("Error creating pipe\n");
 		exit(1);
 	}
 	p_id = fork();
 	if (p_id == -1)
 	{
-		perror("Error forking");
+		perror("Error forking\n");
 		exit(1);
 	}
 	if (p_id == 0)
 	{
-		dup2(fd_in_file, STDIN_FILENO);
-		dup2(p_fd[1], STDOUT_FILENO);
-		execve(cmd_paths[0][i], cmd[0], NULL);
-		close(p_fd[0]);
-		close(p_fd[1]);
+		int	p_id2;
+		int	p_fd2[2];
+		if (pipe(p_fd2) == -1)
+		{
+			perror("Error creating pipe\n");
+			return (1);
+		}
+		p_id2 = fork();
+		if (p_id2 == -1)
+		{
+			perror("Error forking\n");
+			return (1);
+		}
+		if (p_id2 == 0)
+		{
+			close(p_fd[0]);
+			close(p_fd[1]);
+			close(p_fd2[0]);
+			dup2(fd_in_file, STDIN_FILENO);
+			close(fd_in_file);
+			close(fd_out_file);
+			dup2(p_fd2[1], STDOUT_FILENO);
+			close(p_fd2[1]);
+			execve(cmd_paths[0][i], cmd[0], NULL);
+		}
+		else
+		{
+			wait(NULL);
+			close(p_fd[0]);
+			close(p_fd[1]);
+			close(p_fd2[1]);
+			dup2(p_fd2[0], STDIN_FILENO);
+			dup2(fd_out_file, STDOUT_FILENO);
+			close(fd_in_file);
+			close(fd_out_file);
+			close(p_fd2[0]);
+			execve(cmd_paths[1][j], cmd[1], NULL);
+		}
 	}
 	else
 	{
-		close(p_fd[1]);
-		dup2(p_fd[0], STDIN_FILENO);
-		dup2(fd_out_file, STDOUT_FILENO);
-		execve(cmd_paths[1][j], cmd[1], NULL);
+		wait(NULL);
+		close(fd_in_file);
+		close(fd_out_file);
 		close(p_fd[0]);
+		close(p_fd[1]);
+		/*freeing*/
+		free_array(paths);
+		i = 0;
+		while (i < num_of_cmds)
+		{
+			free_array(*(cmd + i));
+			free_array(*(cmd_paths + i));
+			i++;
+		}
+		free(cmd);
+		free(cmd_paths);
 	}
 /* This is not working...*/
 
@@ -200,16 +265,4 @@ int	main(int argc, char **argv, char **envp)
 	// printf("---------\n");
 	// execve(cmd_paths[0][i], cmd[0], NULL);
 	// perror("execve ls");
-
-	/*freeing*/
-	free_array(paths);
-	i = 0;
-	while (i < num_of_cmds)
-	{
-		free_array(*(cmd + i));
-		free_array(*(cmd_paths + i));
-		i++;
-	}
-	free(cmd);
-	free(cmd_paths);
 }
