0d45da7a3d42df8c375ef6ce3a486fc9b5d5f5a9
m-bartos <m.bartos96@seznam.cz>
2023-11-29 15:10:40 +0100

    Update: pipex_utils.c

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 11:51:55 by mbartos           #+#    #+#             */
-/*   Updated: 2023/11/29 14:03:40 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/29 15:09:04 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,4 +69,49 @@ char	*get_path(char *argv, char **env)
 	else
 		path = ft_strdup(paths[i]);
 	return (free_array(paths), path);
+}
+
+char	*strip_newline(char *str)
+{
+	int		len;
+	char	*new_line;
+	int		i;
+
+	if (str == NULL)
+		return (NULL);
+	len = ft_strlen(str);
+	new_line = malloc (len * sizeof(char));
+	i = 0;
+	while (i < len - 1)
+	{
+		new_line[i] = str[i];
+		i++;
+	}
+	new_line[i] = '\0';
+	return (new_line);
+}
+
+void	ft_read_heredoc(char *limiter)
+{
+	char	*line;
+	char	*clean_line;
+	int		heredoc_fd;
+
+	heredoc_fd = open("here_doc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	ft_putstr_fd("heredoc> ", STDOUT_FILENO);
+	line = get_next_line(STDIN_FILENO);
+	clean_line = strip_newline(line);
+	while (ft_strncmp(clean_line, limiter, ft_strlen(limiter)) != 0 \
+		|| ft_strlen(limiter) != ft_strlen(clean_line))
+	{
+		write(heredoc_fd, line, ft_strlen(line) * sizeof(char));
+		ft_putstr_fd("heredoc> ", STDOUT_FILENO);
+		free(clean_line);
+		free(line);
+		line = get_next_line(STDIN_FILENO);
+		clean_line = strip_newline(line);
+	}
+	free(line);
+	free(clean_line);
+	close(heredoc_fd);
 }
