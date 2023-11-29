a70d5b9c33b5a1fb7e56233f594ff01a5d8dc36c
m-bartos <m.bartos96@seznam.cz>
2023-11-29 13:46:15 +0100

    Update: pipex_new.c

--- src/pipex_new.c
+++ src/pipex_new.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 15:02:15 by mbartos           #+#    #+#             */
-/*   Updated: 2023/11/29 13:16:25 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/29 13:45:12 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -190,29 +190,23 @@ int	main(int argc, char **argv, char **env)
 	if (ft_strncmp(argv[1], "here_doc", ft_strlen("here_doc")) == 0 \
 		&& ft_strlen("here_doc") == ft_strlen(argv[1]) && argc > 5)
 	{
-		ft_read_heredoc(argv[2]);
-		fd_in = open("here_doc", O_RDONLY, 0777);
 		i = 3;
+		ft_read_heredoc(argv[2]);
+		fd_in = open("here_doc", O_RDONLY, 0444);
+		fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0666);
 	}
 	else
 	{
-		fd_in = open(argv[1], O_RDONLY, 0777);
 		i = 2;
+		fd_in = open(argv[1], O_RDONLY, 0444);
+		fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	}
-	fd_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (fd_in < 0 || fd_out < 0)
-	{
-		perror("File error");
-		return (1);
-	}
+		return (perror("File error"), 1);
 	dup2(fd_in, STDIN_FILENO);
 	while (i < (argc - 2))
-	{
-		pipe_fork(argv[i], env);
-		i++;
-	}
+		pipe_fork(argv[i++], env);
 	unlink("here_doc");
 	dup2(fd_out, STDOUT_FILENO);
 	execute(argv[i], env);
-	return (1);
 }
